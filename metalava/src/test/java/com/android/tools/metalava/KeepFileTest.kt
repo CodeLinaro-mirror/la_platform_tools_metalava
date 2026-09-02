c106e9cd24a70855a433266324e52d48ac4ec7c1
Paul Duffin <paulduffin@google.com>
2026-09-02 23:14:11 +0000

    Add test for missing public API items in system API keep file
    Add a test in `KeepFileTest` showing that the Proguard keep file
    generated for the system API surface only contains items from the
    target API surface (system API) and omits public API items.
    
    A TODO explains that this is incorrect because the system API keep
    file should retain items from across the whole API surface.
    
    Bug: 512093496
    Test: ./gradlew :metalava:test
    Change-Id: I5e494a120faff95bb48e22406ef250926b6616c1

--- metalava/src/test/java/com/android/tools/metalava/KeepFileTest.kt
+++ metalava/src/test/java/com/android/tools/metalava/KeepFileTest.kt
@@ -393,4 +393,45 @@ class KeepFileTest : DriverTest() {
                 ),
         )
     }
+
+    @Test
+    fun `Proguard file does not contain public API items when generated for system API`() {
+        check(
+            apiSurface = KnownApiSurface.TEST_SYSTEM_API_SURFACE,
+            sourceFiles =
+                arrayOf(
+                    java(
+                        """
+                            package test.pkg;
+
+                            public class PublicClass {
+                                public void publicMethod() {}
+                            }
+                        """
+                    ),
+                    java(
+                        """
+                            package test.pkg;
+
+                            import test.annotation.SystemApi;
+
+                            @SystemApi
+                            public class SystemClass {
+                                public void systemMethod() {}
+                            }
+                        """
+                    ),
+                ),
+            proguard =
+                // TODO: The proguard file should contain all items in the whole API surface (i.e.
+                //  including public API items), but currently only contains items in the target
+                //  API surface.
+                """
+                -keep class test.pkg.SystemClass {
+                    <init>();
+                    public void systemMethod();
+                }
+                """,
+        )
+    }
 }
